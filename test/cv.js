388accc1cc6caf3855a16e13684a9eedcc646609
Nada2S <46956138+Nada2S@users.noreply.github.com>
2019-03-31 16:45:38 +0200

    Add files via upload

---
+++ test/cv.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+//   $("button").click(function(){
+//   	$("#navbar-formation").addClass("center");
+//   	$("#navbar-formation").toggle();
+//     $("#formation").toggle();
+//   });
+
+
+
+// });
